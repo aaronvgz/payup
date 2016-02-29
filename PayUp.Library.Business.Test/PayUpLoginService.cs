a56d01f1439694a139b16e17574786b61f091a0a
aaronvgz <aaron.zabala@hotmail.com>
2016-02-29 09:12:00 +0800

    Code CleanUp

--- PayUp.Library.Business.Test/PayUpLoginService.cs
+++ PayUp.Library.Business.Test/PayUpLoginService.cs
@@ -1,14 +1,25 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
+using NUnit;
 using NUnit.Framework;
+
+using PayUp.Library.Business;
 
 namespace PayUp.Library.Business.Test
 {
 	[TestFixture]
-	public class Tests
+	public class PayUpLoginService
 	{
-		
+		[Test]
+		public void LoginEmailPassword()
+		{
+			Assert.True (
+				Business.Factory.PayUpLogin.Instance().LoginEmailPassword("aaron.zabala@hotmail.com","test"), 
+				this.GetType().ToString());
+			Assert.False (
+				Business.Factory.PayUpLogin.Instance().LoginEmailPassword("xxxxxx@hotmail.com","test"), 
+				this.GetType().ToString());
+		}
+
 	}
 }
 
